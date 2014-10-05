d169807df803bba6bab5a3ec6655e3f88afc5a1d
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-10-05 19:36:17 -0400

    dimension mismatch error msg

--- Python/rvexp/KIC5955122.py
+++ Python/rvexp/KIC5955122.py
@@ -3,6 +3,19 @@ import matplotlib.pyplot as plt
 import pyfits
 from scipy.signal.spectral import lombscargle
 from scipy.signal import periodogram
+import george
+from george.kernels import ExpSquaredKernel, ExpSine2Kernel, WhiteKernel, CosineKernel
+
+plotpar = {'axes.labelsize': 15,
+           'text.fontsize': 20,
+           'legend.fontsize': 15,
+           'xtick.labelsize': 18,
+           'ytick.labelsize': 18,
+           'text.usetex': True}
+plt.rcParams.update(plotpar)
+
+ocols = ['#FF9933','#66CCCC' , '#FF33CC', '#3399FF', '#CC0066',
+'#99CC99', '#9933FF', '#CC0000', '#99CC00']
 
 lc_file = "/Users/angusr/.kplr/data/lightcurves/005955122/kplr005955122-2009166044711_slc.fits"
 hdulist = pyfits.open(lc_file)
@@ -14,16 +27,52 @@ q = tbdata["SAP_QUALITY"]
 x -= x[0]
 n = np.isfinite(x)*np.isfinite(y)*np.isfinite(yerr)*(q==0) * (x < 1)
 x, y, yerr = x[n], y[n], yerr[n]
+y -= np.median(y)
 
+plt.clf()
 plt.subplot(2,1,1)
 plt.errorbar(x, y, yerr=yerr, fmt='k.', capsize=0, ecolor='.8')
 
+theta = [2e5, .05, 10, 1000, .1]
+xs = np.linspace(min(x), max(x), 100)
+
+# k = kernel(theta)
+k = theta[0] * ExpSquaredKernel(theta[1]) * ExpSine2Kernel(theta[2], theta[4])
+k += WhiteKernel(theta[3])
+gp = george.GP(k)
+gp.compute(x, np.sqrt(theta[3]+yerr**2))
+print gp.lnlikelihood(y, quiet=True)
+
+print theta
+mu, cov = gp.predict(y, xs)
+pars, results = gp.optimize(x, y, yerr)
+print pars
+
+k = theta[0] * ExpSquaredKernel(theta[1]) * ExpSine2Kernel(theta[2], theta[4])
+k += WhiteKernel(theta[3])
+gp = george.GP(k)
+gp.compute(x, np.sqrt(theta[3]+yerr**2))
+
+nmu, ncov = gp.predict(y, xs)
+
+plt.plot(xs, mu, 'b-')
+plt.plot(xs, nmu, color=ocols[1])
+plt.ylabel('$\mathrm{Flux}$')
+
+def predict(xs):
+    return gp.predict(y, xs)[0]
+
+from scipy.misc import derivative
+derivs = derivative(predict, xs)
+
 plt.subplot(2,1,2)
-
-ps = periodogram(y)
-plt.plot(ps[0], ps[1], 'b-')
-# plt.xlim(0, .1)
-plt.ylim(0, 1e7)
-plt.xlabel('frequency')
-
-plt.show()
+rvx = xs
+rv_err = .5 + np.random.rand(len(rvx))
+rvs = derivs/100. #+ np.random.uniform(-1, 1)*rv_err
+rvs += np.random.randn(len(rvx)) * rv_err
+plt.errorbar(xs, rvs, yerr=rv_err, fmt='k.',
+             capsize=0, ecolor='.8')
+plt.plot(rvx, derivs/100., color=ocols[2])
+plt.xlabel('$\mathrm{Time~(Days)}$')
+plt.ylabel('$\mathrm{RV~(ms}_{-1}\mathrm{)}$')
+plt.savefig('rvs')
--- Python/rvexp/fit_rv.py
+++ Python/rvexp/fit_rv.py
@@ -6,8 +6,14 @@ from scipy.signal import periodogram
 import george
 from george.kernels import ExpSquaredKernel, ExpSine2Kernel, WhiteKernel, CosineKernel
 from scipy.misc import derivative
+from scipy.optimize import minimize, fmin
 
-def predict(xs):
+def predict(xs, theta, y, yerr):
+    k = theta[0] * ExpSquaredKernel(theta[1]) * ExpSine2Kernel(theta[2], theta[4])
+    k += WhiteKernel(theta[3])
+    gp = george.GP(k)
+    print type(yerr), np.shape(yerr), type(y), np.shape(y)
+    gp.compute(xs, yerr)
     return gp.predict(y, xs)[0]
 
 def fit_rv(theta, x, y, yerr, xs):
@@ -30,17 +36,20 @@ def fit_rv(theta, x, y, yerr, xs):
     plt.plot(xs, mu, 'r')
     plt.savefig('init_rv_fit')
 
-    pars, res = gp.optimize(x, y, yerr)
-    print pars, 'pars'
+#     pars, res = gp.optimize(x, y, yerr)
+#     results = fmin(neglnlike, theta, args=(x, y, yerr))
+#     print results
+#     print pars, 'pars'
+#     raw_input('enter')
 
-    k = pars[0] * ExpSquaredKernel(pars[1]) * ExpSine2Kernel(pars[2], pars[4])
-    k += WhiteKernel(pars[3])
-    gp = george.GP(k)
-    gp.compute(x, yerr)
-    print "final likelihood = ", gp.lnlikelihood(y, quiet=True)
+#     k = pars[0] * ExpSquaredKernel(pars[1])  # * ExpSine2Kernel(pars[2], pars[4])
+#     k += WhiteKernel(pars[2])
+#     gp = george.GP(k)
+#     gp.compute(x, yerr)
+#     print "final likelihood = ", gp.lnlikelihood(y, quiet=True)
 
-    mu, cov = gp.predict(y, xs)
+#     mu, cov = gp.predict(y, xs)
 
-    derivs = derivative(predict, xs)
+    derivs = derivative(predict, xs, args=(theta, y, yerr))
 
     return mu, cov, derivs
