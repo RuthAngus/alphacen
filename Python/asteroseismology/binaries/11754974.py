bef3a6f9b931f36f5a7ea9286024345d157ab35d
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-11-17 13:56:47 -0500

    adding uncertainties

--- Python/asteroseismology/binaries/11754974.py
+++ Python/asteroseismology/binaries/11754974.py
@@ -13,9 +13,10 @@ ocols = plot_colours()
 def testing():
     x = np.arange(0, 10, .1)
     y = np.sin(2*x + np.pi/4.)
+    yerr = np.ones_like(y)*.01
     plt.clf()
     plt.plot(x, y)
-    ys, A = fit_single_sine(x, y, 2)
+    ys, A = fit_single_sine_err(x, y, yerr, 2)
     plt.plot(x, ys, 'r')
     plt.show()
     print find_phase(A)/np.pi
@@ -37,23 +38,24 @@ def pgram(x, y, peak1):
     raw_input('enter')
 
 # find the phases of each segment
-def all_phases(xl, yl, peak):
+def all_phases(xl, yl, yerrl, peak):
     nsegs = len(xl)
     phis = np.zeros(nsegs)
     for i in range(nsegs):
         if len(xl[i]):  # make sure you don't have an empty array
-            ysl, Al = fit_single_sine(xl[i], yl[i], peak)
+            ysl, Al = fit_single_sine_err(xl[i], yl[i], yerrl[i], peak)
             phis[i] = find_phase(Al)
     return phis
 
 # returns list of lists of the full time series broken down into nsegs
 # ndays long
-def segment(x, y, ndays, nsegs):  # nseg is zero counted
-    xs, ys = [], []
+def segment(x, y, yerr, ndays, nsegs):  # nseg is zero counted
+    xs, ys, yerrs = [], [], []
     for seg in range(nsegs):
         l = (x[0]+ndays*seg < x) * (x < x[0]+ndays*seg+ndays)
         xs.append(x[l])
         ys.append(y[l])
+        yerrs.append(yerr[l])
 
     BJD = np.zeros(len(xs))
     for i in range(len(xs)):
@@ -64,11 +66,23 @@ def segment(x, y, ndays, nsegs):  # nseg is zero counted
             BJD[i] = xs[i-n][0] + 10 * n
             n += 1
 
-    return xs, ys, BJD
+    return xs, ys, yerrs, BJD
 
 def light_travel_time(phis, nu):
     phi_mean = np.mean(phis)
     return (phis - phi_mean) / (2*np.pi*nu)
+
+def fit_single_sine_err(x, y, yerr, w):
+    M = np.ones((len(x), 2+1))
+    S = yerr**2 * np.eye(len(yerr))
+    M[:, 0] = np.sin(w*x)
+    M[:, 1] = np.cos(w*x)
+    A = np.linalg.solve(M.T.dot(np.linalg.inv(S)).dot(M),
+                        (M.T.dot(np.linalg.inv(S)).dot(y)))
+    ys = np.zeros_like(x)
+    ys = A[0]*np.sin(w*x) + A[1]*np.cos(w*x)
+    ys += A[2]
+    return ys, A
 
 def fit_single_sine(x, y, w):
     M = np.ones((len(x), 2+1))
@@ -84,7 +98,7 @@ def find_phase(A):
      return np.arctan(A[1]/A[0])
 
 # load data, median normalise and join together
-def load_join(KID, sc=False):
+def load_join(KID, nquarters, sc=False):
     if sc == True:
         lc_files = \
                 glob.glob("/Users/angusr/.kplr/data/lightcurves/%s/*_slc.fits"
@@ -112,6 +126,8 @@ def load_join(KID, sc=False):
             x = np.concatenate((x, t))
             y = np.concatenate((y, flux))
             yerr = np.concatenate((yerr, flux_err))
+        if i == nquarters-1:
+            break
 
     # convert ys to float64
     y2 = np.empty((len(y)))
@@ -125,7 +141,7 @@ if __name__ == "__main__":
 
     # load data
     KID = "11754974"
-    x, y, yerr = load_join(KID)
+    x, y, yerr = load_join(KID, 15)
 
 #     plt.clf()
 #     plt.errorbar(x, y, yerr=yerr, **reb)
@@ -135,25 +151,27 @@ if __name__ == "__main__":
     peak1, peak2, peak3 = 16.34, 21.40, 20.91
 
     # divide data into nsegs segments of ndays days
-    nsegs, ndays = int((max(x)-min(x))/10), 10
-    print nsegs, 'nsegs'
-    xl, yl, BJD = segment(x, y, ndays, nsegs)
+    ndays = 10
+    nsegs = int((max(x)-min(x))/ndays)
 
-    phis1 = all_phases(xl, yl, peak1)
+    print nsegs, 'nsegs'
+    xl, yl, yerrl, BJD = segment(x, y, yerr, ndays, nsegs)
+
+    phis1 = all_phases(xl, yl, yerrl, peak1)
     print phis1, np.mean(phis1)
     times1 = light_travel_time(phis1, peak1) * 24*3600
-    phis2 = all_phases(xl, yl, peak2)
+    phis2 = all_phases(xl, yl, yerrl, peak2)
     times2 = light_travel_time(phis2, peak2) * 24*3600
-    phis3 = all_phases(xl, yl, peak3)
+    phis3 = all_phases(xl, yl, yerrl, peak3)
     times3 = light_travel_time(phis3, peak3) * 24*3600
 
     times = np.vstack((times1, times2, times3))
     mean_times = np.mean(times, axis=0)
 
     plt.clf()
-    plt.plot(BJD, phis1, '.')
-#     plt.plot(BJD, times1, '.')
-#     plt.plot(BJD, times2, '.')
-#     plt.plot(BJD, times3, '.')
-#     plt.plot(BJD, mean_times)
+#     plt.plot(BJD, phis1, '.')
+    plt.plot(BJD, times1, '.')
+    plt.plot(BJD, times2, '.')
+    plt.plot(BJD, times3, '.')
+    plt.plot(BJD, mean_times)
     plt.show()
